0350022bd003c3ed9f8f4ef830e491f319d88251
Sam Cole <scole3@uic.edu>
2018-05-14 21:12:03 -0500

    Add functions plot_eigs() and test_eigs() and main block for testing

--- rk1pert.py
+++ rk1pert.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+import sys
 
 def rand_unit_vector(n):
     """Return a random unit vector uniformly distributed on unit sphere"""
@@ -26,3 +28,35 @@ def rand_rk1_proj(n):
 def pencil(A0, A1):
     """Return a funtion A(z) = A0 + z * A1"""
     return lambda z: A0 + z * A1
+
+def plot_eigs(A, tmax, tstep, filename):
+    """Plot the eigenvalues of the Hermitian pencil A(t) for t in [0, tmax]"""
+    x = np.arange(0, tmax, tstep)
+    y = {t: np.linalg.eigvalsh(A(t)) for t in x}
+    n = A(0).shape[0]
+    for i in range(n):
+        plt.plot(x, [y[t][i] for t in x])
+    plt.xlabel(r"$t$")
+    plt.ylabel(r"Eigenvalues of $A(t)$")
+    plt.savefig(filename)
+
+def test_eigs(n, dmin, dmax, tmax, tstep):
+    """Plot the eigenvalues of a randomly generated Hermitian pencil with given parameters"""
+    D = rand_diag(n, dmin, dmax)
+    U = rand_rk1_proj(n)
+    A = pencil(D, U)
+    plot_eigs(A, tmax, tstep, "output.pdf")
+
+
+
+if __name__ == "__main__":
+
+    try:
+        n = int(sys.argv[1])
+        (dmin, dmax, tmax, tstep) = (float(a) for a in sys.argv[2:])
+    except(ValueError, TypeError):
+        print("ERROR ERROR ERROR!!!")
+        print("Usage: python rk1pert.py n dmin dmax tmax tstep")
+        exit()
+
+    test_eigs(n, dmin, dmax, tmax, tstep)
